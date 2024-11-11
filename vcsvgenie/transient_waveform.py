865f68b9c200ef6af6cc498f9d6918d122ed6bcf
FSharp4 <33583307+FSharp4@users.noreply.github.com>
2024-11-11 03:58:35 -0800

    Detection of propagations and measurement of delays

--- vcsvgenie/transient_waveform.py
+++ vcsvgenie/transient_waveform.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Dict, List
+from typing import Dict, List, Literal
 from warnings import warn
 from matplotlib import pyplot as plt
 import numpy as np
@@ -12,6 +12,24 @@ class WaveForm:
     x: NDArray[np.float64]
     y: NDArray[np.float64]
     title: str
+
+@dataclass
+class Transition:
+    TransitionType = Literal['Rising', 'Falling']
+    transition_type: TransitionType
+    series: str
+    series_type: Literal['Input', 'Output']
+    interval: int
+
+@dataclass
+class Propagation:
+    source: str
+    destination: str
+    departure: float
+    arrival: float
+    propagation_type: Literal['Rising', 'Falling']
+    delay: float
+    interval: int
 
 class TransientResult:
     def __init__(self, inputs: List[WaveForm], outputs: List[WaveForm], name: str = "Transient Results"):
@@ -31,7 +49,33 @@ class TransientResult:
             self.outputs[output.title] = output.y
 
         self.name = name
+        self.timestep = 1e-9
+        self.eps_n_timestamps = 10
+        self._transitions: List[List[Transition]] = list()
+        self._interval_start_idxs: NDArray[np.int32] = np.zeros(1, dtype=np.int32)
+        self._interval_end_idxs: NDArray[np.int32] = np.zeros(1, dtype=np.int32)
+    
+    @property
+    def transitions(self) -> List[List[Transition]]:
+        if len(self._transitions) == 0:
+            raise Exception("Transitions not calculated (call TransientResult.find_transitions first)")
+    
+        return self._transitions
+
+    @property
+    def interval_start_idxs(self) -> NDArray[np.int32]:
+        if np.all(self._interval_start_idxs == 0):
+            raise Exception("Interval Start Timestamp Indexes not calculated (call TransientResult.find_transitions first)")
         
+        return self._interval_start_idxs
+
+    @property
+    def interval_end_idxs(self) -> NDArray[np.int32]:
+        if np.all(self._interval_end_idxs == 0):
+            raise Exception("Interval End Timestamp Indexes not calculated (call TransientResult.find_transitions first)")
+        
+        return self._interval_end_idxs
+    
     def plot(self, save: bool = False, display: bool = True, separate: bool = False) -> None:
         if not display and not save:
             warn("TransientResult.plot called without save nor display; defaulting to display")
@@ -68,7 +112,7 @@ class TransientResult:
 
             for title in self.outputs:
                 plt.figure()
-                plt.plot(self.timestamps, self.inputs[title], 'r')
+                plt.plot(self.timestamps, self.outputs[title], 'r')
                 plt.xlabel("Timestamp (s)")
                 plt.ylabel("Voltage (V)")
                 plt.title(f"{self.name} {title}")
@@ -79,8 +123,113 @@ class TransientResult:
                     plt.savefig(f"TransientResult__plot {self.name}_{title.replace('/', '_')}.png")
                     plt.close()
 
-    def find_propagations(timestep_s: 1e-9):
-        pass
+    def find_transitions(self, timestep: float = 1e-9, eps_n_timestamps: int = 1, LOGIC_THRESHOLD: float = 0.5) -> None:
+        self.timestep = timestep
+        self.eps_n_timestamps = eps_n_timestamps
+        ending_timestamp = self.timestamps[-1]
+        n_iterations = np.int32((ending_timestamp + timestep/2) // timestep) - 1
+
+        start_timestamp_idxs = np.zeros(n_iterations, dtype=np.int32)
+        end_timestamp_idxs = np.zeros(n_iterations, dtype=np.int32)
+        transitions: List[List[Transition]] = list()
+        for n in range(n_iterations):
+            start = timestep * n
+            end = timestep * (n + 1)
+            start_timestamp_idxs[n] = np.searchsorted(self.timestamps, start, side='right')
+            end_timestamp_idxs[n] = np.searchsorted(self.timestamps, end, side='right') - eps_n_timestamps
+            transitions.append([])
+
+        for input_series in self.inputs.keys():
+            data = self.inputs[input_series]
+            starts_data = data[start_timestamp_idxs] > LOGIC_THRESHOLD
+            ends_data = data[end_timestamp_idxs] > LOGIC_THRESHOLD
+            
+            check_transitioned = ends_data != starts_data
+            for idx, did_transition in enumerate(check_transitioned):
+                if did_transition:
+                    transition_type = 'Rising' if ends_data[idx] else 'Falling'
+                    transition = Transition(transition_type, input_series, 'Input', idx)
+                    transitions[idx].append(transition)
+        
+        for output_series in self.outputs.keys():
+            data = self.outputs[output_series]
+            starts_data = data[start_timestamp_idxs] > LOGIC_THRESHOLD
+            ends_data = data[end_timestamp_idxs] > LOGIC_THRESHOLD
+            
+            check_transitioned = ends_data != starts_data
+            for idx, did_transition in enumerate(check_transitioned):
+                if did_transition:
+                    transition_type = 'Rising' if ends_data[idx] else 'Falling'
+                    transition = Transition(transition_type, output_series, 'Output', idx)
+                    transitions[idx].append(transition)
+        
+        self._transitions = transitions
+        self._interval_start_idxs = start_timestamp_idxs
+        self._interval_end_idxs = end_timestamp_idxs
+
+    def find_propagations(self, LOGIC_THRESHOLD: float = 0.5):
+        propagations: List[Propagation] = list()
+        for idx, timestep_transitions in enumerate(self.transitions):
+            if len(timestep_transitions) < 2:
+                continue
+
+            input_transitions: List[Transition] = [transition for transition in timestep_transitions if transition.series_type == 'Input']
+            output_transitions: List[Transition] = [transition for transition in timestep_transitions if transition.series_type == 'Output']
+            if len(output_transitions) == 0:
+                continue
+
+            for input_transition in input_transitions:
+                input_transition_timestamp = self.interpolate_transition_timestamp(LOGIC_THRESHOLD, input_transition)
+                for output_transition in output_transitions:
+                    output_transition_timestamp = self.interpolate_transition_timestamp(LOGIC_THRESHOLD, output_transition)
+                    propagation_delay = output_transition_timestamp - input_transition_timestamp
+                    propagations.append(
+                        Propagation(
+                            input_transition.series, 
+                            output_transition.series, 
+                            input_transition_timestamp, 
+                            output_transition_timestamp, 
+                            output_transition.transition_type, 
+                            propagation_delay, 
+                            input_transition.interval
+                        )
+                    )
+            
+        return propagations
+
+    def interpolate_transition_timestamp(self, LOGIC_THRESHOLD: float, transition: Transition) -> float:
+        sweep_start_timestamp: np.int32 = self.interval_start_idxs[transition.interval]
+        sweep_end_timestamp: np.int32 = self.interval_end_idxs[transition.interval] + 1
+        data_interval: NDArray[np.float64]
+        if transition.series_type == 'Input':
+            data_interval = self.inputs[transition.series][sweep_start_timestamp:sweep_end_timestamp]
+        else:
+            data_interval = self.outputs[transition.series][sweep_start_timestamp:sweep_end_timestamp]
+
+        check_transitioned = data_interval > LOGIC_THRESHOLD
+        if check_transitioned[0]:
+            check_transitioned = np.logical_not(check_transitioned)
+                
+        deltastamp: int = 0
+        for idx, is_transitioned in enumerate(check_transitioned):
+            if not is_transitioned:
+                deltastamp = idx
+                    
+        x_low: int = sweep_start_timestamp + deltastamp
+        x_high: int = x_low + 1
+        y_low: np.float64 = data_interval[deltastamp]
+        y_high: np.float64 = data_interval[deltastamp + 1]
+        proportion: float = (LOGIC_THRESHOLD - y_low) / (y_high - y_low)
+        time_low = self.timestamps[x_low]
+        time_high = self.timestamps[x_high]
+        interpolated_timestamp: float = time_low + (time_high - time_low) * proportion
+        return interpolated_timestamp              
+
+
+def find_transitions(transient_result: TransientResult, timestep: float = 1e-9, eps_n_timestamps: int = 10) -> List[List[Transition]]:
+    transient_result.find_transitions(timestep=timestep, eps_n_timestamps=eps_n_timestamps)[0]
+    return transient_result.transitions
+
         
 def construct_waveforms(waveform_dataframe: DataFrame, titles: List[str]) -> List[WaveForm]:
     waveforms: List[WaveForm] = list()
